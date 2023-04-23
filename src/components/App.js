4201f267d53946bdb1af2a136e6361ff60da61a1
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-04-23 17:49:43 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => setImageUrl(data.message));
+  }, []);
+
+  if (!imageUrl) return <p>Loading ...</p>;
+  return <img src={imageUrl} alt="A Random Dog" />;
+}
+
+export default App;
+
